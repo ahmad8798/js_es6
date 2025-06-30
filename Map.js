27629d62554621dac1c59b5859a8be9394cceb8a
ahmad8798 <shaikahmadpasha8798@gmail.com>
2025-06-30 18:40:19 +0530

    Task is created on Js Map method

---
+++ Map.js
@@ -0,0 +1,7 @@
+const users = [
+  { id: 1, name: "Alice", age: 25 },
+  { id: 2, name: "Bob", age: 30 },
+  { id: 3, name: "Charlie", age: 22 }
+];
+
+//Create a new array that contains only the names of the users in uppercase letters.
